718c58010216a82a3164e2d375cea76f04c69986
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-09-21 12:32:43 -0400

    Created a server with mcpserver

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,17 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 
+const server = new McpServer({
+  name: "test",
+  version: "1.0.0",
+  capabilities: {
+    resources: {},
+    tools: {},
+    prompts: {},
+  }
+});
 
+function main (){
+  console.log("apples");
+  
+}
+main()
