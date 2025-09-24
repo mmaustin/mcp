27af3cb7d3e286415b49f90708bacb3880c8144d
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-09-24 18:21:04 -0400

    Installed zod and implemented server.tool

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+
 
 const server = new McpServer({
   name: "test",
@@ -11,7 +13,19 @@ const server = new McpServer({
   }
 });
 
-async function main (){
+server.tool("create-user", "Create a new user in the database", {
+  name: z.string(),
+  email: z.string(),
+  address: z.string(),
+  phone: z.string()
+}, {
+  title: "Create User",
+
+}, async () => {
+  return {}
+})
+
+async function main() {
   const transport = new StdioServerTransport();
   await server.connect(transport);
 }
