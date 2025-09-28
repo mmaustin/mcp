a2ca53183610701618f526d46b0584aabfe7f591
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-09-28 11:44:24 -0400

    Wrote the tool's accompanying function

--- src/server.ts
+++ src/server.ts
@@ -29,11 +29,44 @@ server.tool("delete-user", "Delete the las user from the database", {}, {title: 
         content: [
           {type: "text", text: "Failed to delete user"}
         ]
-      }
-      
+      }     
     }
   }
 );
+
+async function deleteUser(){
+  const users = await import("./data/user.json", {
+    with: {type: "json"}
+  }).then(m => m.default);
+
+  const id = users.length;
+
+  users.pop();
+
+  await fs.writeFile("./src/data/user.json", JSON.stringify(users, null, 2));
+  
+  return id;
+}
+
+async function createUser(user: {
+  name: string,
+  email: string,
+  address: string,
+  phone: string
+}) {
+  const users = await import("./data/user.json", {
+    with: {type: "json"}
+  }).then(m => m.default);
+  
+  const id = users.length + 1;
+  
+  users.push({id, ...user});
+  
+  await fs.writeFile("./src/data/user.json", JSON.stringify(users, null, 2));
+  
+  return id;
+  
+};
 
 server.tool("create-user", "Create a new user in the database", {
   name: z.string(),
@@ -62,26 +95,6 @@ server.tool("create-user", "Create a new user in the database", {
     }
   }
 });
-
-async function createUser(user: {
-  name: string,
-  email: string,
-  address: string,
-  phone: string
-}) {
-  const users = await import("./data/user.json", {
-    with: {type: "json"}
-  }).then(m => m.default);
-
-  const id = users.length + 1;
-  
-  users.push({id, ...user});
-
-  await fs.writeFile("./src/data/user.json", JSON.stringify(users, null, 2));
-
-  return id;
-  
-};
 
 async function main() {
   const transport = new StdioServerTransport();
