681e560cfd64be26dde6b08fdea3e8f4ee12e727
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-10-12 16:07:56 -0400

    Completed helper function for making api calls to weather services

---
+++ src/weather.ts
@@ -0,0 +1,40 @@
+import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+
+const nws_api_base = "https://api.weather.gov";
+const USER_AGENT = "weather-app/1.0";
+
+const server = new McpServer({
+  name: "weather",
+  version: "1.0.0",
+  capabilities: {
+    resources: {},
+    tools: {},
+  },
+});
+
+async function makeNWSRequest<T>(url: string): Promise<T | null> {
+  const headers = {
+    "User-Agent": USER_AGENT,
+    Accept: "application/geo+json",
+  };
+
+  try {
+    const response = await fetch(url, {headers});
+    if(!response.ok){
+      throw new Error(`HTTP error! stats: ${response.status}.`);
+    };
+    return (await response.json()) as T;
+  } catch (error) {
+    console.error("Error making NWS request:", error);
+    return null;
+  }
+
+}
+
+async function main() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+}
+main();
